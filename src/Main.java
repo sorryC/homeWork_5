d3ad5189c955f372f669ffc82edc34346bc10316
Vakulenko <og4youforever@gmail.com>
2022-08-31 17:44:53 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 0;
+        while (i < 10) {
+            System.out.println(++i + " ");
+        }
+        System.out.println();
+
+        for (; i > 0; --i) {
+            System.out.println(i + " ");
+        }
     }
 }
