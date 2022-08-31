dde6eb47efca5c57fa2ad5c3e7822bab009a6260
Vakulenko <og4youforever@gmail.com>
2022-08-31 17:53:39 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int i = 5;
         for (int b = i; b < 31; b += 7) {
-            System.out.println("Сегодня пятница " + b + " -e число" + " нужен отчет!" );
+            System.out.println("Сегодня  " + b + " -e число" + " нужен отчет!" );
         }
     }
 }
